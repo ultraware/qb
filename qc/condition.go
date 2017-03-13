a56701f4af7c0e7e7c1503a28573d326477a305c
NiseVoid <nisevoid@gmail.com>
2017-03-13 15:25:05 +0100

    Accept non-field arguments in qc functions

--- qc/condition.go
+++ qc/condition.go
@@ -2,7 +2,17 @@ package qc
 
 import "git.ultraware.nl/NiseVoid/qb"
 
-func createCondition(f1, f2 qb.Field, action func(f ...string) string) qb.Condition {
+func createCondition(i1, i2 interface{}, action func(f ...string) string) qb.Condition {
+	var f1, f2 qb.Field
+	var ok bool
+
+	if f1, ok = i1.(qb.Field); !ok {
+		f1 = qb.Value(i1)
+	}
+	if f2, ok = i2.(qb.Field); !ok {
+		f2 = qb.Value(i2)
+	}
+
 	if f1.DataType() != f2.DataType() {
 		panic(`Types don't match. ` + f1.DataType() + ` and ` + f2.DataType())
 	}
@@ -10,26 +20,26 @@ func createCondition(f1, f2 qb.Field, action func(f ...string) string) qb.Condit
 }
 
 // Eq ...
-func Eq(f1, f2 qb.Field) qb.Condition {
+func Eq(f1, f2 interface{}) qb.Condition {
 	return createCondition(f1, f2, func(f ...string) string { return f[0] + ` = ` + f[1] })
 }
 
 // Gt ...
-func Gt(f1, f2 qb.Field) qb.Condition {
+func Gt(f1, f2 interface{}) qb.Condition {
 	return createCondition(f1, f2, func(f ...string) string { return f[0] + ` > ` + f[1] })
 }
 
 // Gte ...
-func Gte(f1, f2 qb.Field) qb.Condition {
+func Gte(f1, f2 interface{}) qb.Condition {
 	return createCondition(f1, f2, func(f ...string) string { return f[0] + ` >= ` + f[1] })
 }
 
 // Lt ...
-func Lt(f1, f2 qb.Field) qb.Condition {
+func Lt(f1, f2 interface{}) qb.Condition {
 	return createCondition(f1, f2, func(f ...string) string { return f[0] + ` < ` + f[1] })
 }
 
 // Lte ...
-func Lte(f1, f2 qb.Field) qb.Condition {
+func Lte(f1, f2 interface{}) qb.Condition {
 	return createCondition(f1, f2, func(f ...string) string { return f[0] + ` <= ` + f[1] })
 }
