76f6785864d13e22f688c476baff5c226b53fa8e
NiseVoid <nisevoid@gmail.com>
2017-06-19 23:52:31 +0200

    Add qb-lint

---
+++ qb-lint/main.go
@@ -0,0 +1,328 @@
+package main
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+	"go/types"
+	"os"
+	"path"
+	"strings"
+
+	"golang.org/x/tools/go/loader"
+)
+
+const aliasLimit = 6
+
+var fset *token.FileSet
+var info = &types.Info{
+	Types: make(map[ast.Expr]types.TypeAndValue),
+}
+
+func fatal(args ...interface{}) {
+	fmt.Println(args...)
+	os.Exit(2)
+}
+
+func main() {
+	fixArgs()
+	okay := true
+
+	var conf loader.Config
+	conf.ImportWithTests(os.Args[1])
+
+	prog, err := conf.Load()
+	if err != nil {
+		fatal(err)
+	}
+
+	pkg := prog.Package(os.Args[1])
+
+	fset = prog.Fset
+	info = &pkg.Info
+
+	for _, v := range pkg.Files {
+		parseFile(v)
+	}
+
+	if !okay {
+		os.Exit(1)
+	}
+}
+
+func fixArgs() {
+	var target string
+	if len(os.Args) == 1 || os.Args[1] == `` {
+		target = `.`
+	} else {
+		target = os.Args[1]
+	}
+
+	if target[0] == '.' {
+		gopath := ``
+		cwd, _ := os.Getwd()
+
+		target = path.Clean(path.Join(cwd, target))
+
+		for _, v := range strings.Split(os.Getenv(`GOPATH`), `:`) {
+			path := path.Join(v, `src`)
+			if strings.HasPrefix(cwd, path) {
+				gopath = path
+			}
+		}
+
+		target = strings.TrimPrefix(strings.TrimPrefix(target, gopath), `/`)
+	}
+	os.Args = append(os.Args[:1], target)
+}
+
+type field struct {
+	table  ast.Ident
+	column ast.Ident
+}
+
+func (f field) String() string {
+	return f.table.Name + `.` + f.column.Name
+}
+
+func (f field) PositionString() string {
+	pos := fset.Position(f.table.Pos())
+	return pos.String()
+}
+
+var usedFields map[field]bool
+
+func parseFile(file *ast.File) bool {
+	okay := true
+
+	for _, v := range file.Decls {
+		f, ok := v.(*ast.FuncDecl)
+		if !ok {
+			continue
+		}
+
+		usedFields = make(map[field]bool)
+		translate = make(map[interface{}]*ast.Ident)
+
+		if f.Name.Obj == nil {
+			continue
+		}
+		initialFunc = f.Name.Obj.Decl
+		currentFunc = initialFunc
+
+		ast.Inspect(f, findTables)
+		ast.Inspect(f, findSelected)
+		ast.Inspect(f, findUsed)
+
+		for k, v := range usedFields {
+			if v {
+				continue
+			}
+			fmt.Println(k.PositionString()+`:Field`, k, `retrieved but never used`)
+			okay = false
+		}
+	}
+
+	return okay
+}
+
+var initialFunc interface{}
+var currentFunc interface{}
+
+var translate map[interface{}]*ast.Ident
+
+func tryFunc(n ast.Node, f func(ast.Node) bool) {
+	if v, ok := n.(*ast.CallExpr); ok {
+		i, ok := v.Fun.(*ast.Ident)
+		if !ok || i.Obj == nil || i.Obj.Decl == nil {
+			return
+		}
+		if i.Obj.Decl == initialFunc {
+			return
+		}
+		if fmt.Sprint(i.Obj.Kind) == `func` {
+			param := 0
+			for _, p := range i.Obj.Decl.(*ast.FuncDecl).Type.Params.List {
+				for _, i := range p.Names {
+					if _, ok := v.Args[param].(*ast.Ident); !ok {
+						continue
+					}
+					translate[i.Obj.Decl] = v.Args[param].(*ast.Ident)
+					param++
+				}
+			}
+
+			oldFunc := currentFunc
+			currentFunc = i.Obj.Decl
+			ast.Inspect(i.Obj.Decl.(*ast.FuncDecl), f)
+
+			currentFunc = oldFunc
+		}
+	}
+}
+
+func findTables(n ast.Node) bool {
+	assign, ok := n.(*ast.AssignStmt)
+	if !ok {
+		return true
+	}
+	if len(assign.Lhs) != len(assign.Rhs) {
+		return false
+	}
+	for k := range assign.Lhs {
+		if t, ok := tryQbStruct(assign.Rhs[k]); !ok || !checkQbStruct(t) {
+			continue
+		}
+		rhs := assign.Lhs[k].(*ast.Ident)
+		if len(rhs.Name) > aliasLimit {
+			fmt.Printf("%v:Found long alias '%s' (length %d). An alias should be short (<= %d)\n",
+				fset.Position(rhs.Pos()), rhs.Name, len(rhs.Name), aliasLimit)
+		}
+	}
+	return false
+}
+
+func findSelected(n ast.Node) bool {
+	if expr, ok := n.(ast.Expr); ok {
+		return traverseExpr(nil, expr)
+	}
+	return true
+}
+
+var skip int
+
+func findUsed(n ast.Node) bool {
+	if len(usedFields) > 0 {
+		tryFunc(n, findUsed)
+	}
+
+	if skip > 0 {
+		skip--
+		return true
+	}
+	if _, ok := n.(*ast.AssignStmt); ok {
+		skip = 1
+		return true
+	}
+
+	return tryField(n)
+}
+
+func traverseExpr(parent ast.Expr, e ast.Expr) bool {
+	if e == nil {
+		return false
+	}
+	if expr, ok := e.(*ast.SelectorExpr); ok {
+		switch fmt.Sprint(expr.Sel) {
+		case `Returning`:
+			checkFields(parent, 1)
+		case `Select`:
+			checkFields(parent, 0)
+		default:
+			return traverseExpr(expr, expr.X)
+		}
+		return false
+	}
+
+	call, ok := e.(*ast.CallExpr)
+	if !ok {
+		return true
+	}
+
+	return traverseExpr(e, call.Fun)
+}
+
+func checkFields(e ast.Expr, skip int) {
+	call := e.(*ast.CallExpr)
+	args := call.Args[skip:]
+
+	for _, v := range args {
+		field := getField(v)
+		if field == nil {
+			continue
+		}
+
+		usedFields[*field] = false
+	}
+}
+
+func getField(e ast.Expr) *field {
+	if expr, ok := e.(*ast.SelectorExpr); ok {
+		if t, ok := tryQbStruct(expr.X); !ok || !checkQbStruct(t) {
+			// False positive. The found type is not a type from qb-generator
+			return nil
+		}
+
+		return &field{
+			table:  *expr.X.(*ast.Ident),
+			column: *expr.Sel,
+		}
+	}
+	return nil
+}
+
+func tryQbStruct(e ast.Expr) (*types.Struct, bool) {
+	t := info.TypeOf(e)
+	ptr, ok := t.(*types.Pointer)
+	if !ok {
+		return nil, false
+	}
+	t = ptr.Elem().Underlying()
+	s, ok := t.(*types.Struct)
+	return s, ok
+}
+
+func checkQbStruct(t *types.Struct) bool {
+	if t.Field(0).Name() != `Data` {
+		return false
+	}
+	if t.Field(t.NumFields()-1).Name() != `table` {
+		return false
+	}
+	return true
+}
+
+func tryField(n ast.Node) bool {
+	expr, ok := n.(*ast.SelectorExpr)
+	if !ok {
+		return true
+	}
+
+	sel, ok := expr.X.(*ast.SelectorExpr)
+	if !ok {
+		return true
+	}
+	if sel.Sel.Name != `Data` {
+		return false
+	}
+	x := sel.X.(*ast.Ident)
+	if _, ok := x.Obj.Decl.(*ast.Field); ok {
+		if new, ok := translate[x.Obj.Decl]; ok {
+			x = new
+		} else {
+			return false
+		}
+	}
+
+	found := false
+	for k := range usedFields {
+		if x.Obj.Decl != k.table.Obj.Decl || // table object doesn't match
+			expr.Sel.Name != k.column.Name || // field name doesn't match
+			expr.X.Pos() < k.table.Pos() { // The field was used before it was selected
+			continue
+		}
+
+		usedFields[k] = true
+		found = true
+	}
+	if found {
+		return false
+	}
+	if len(usedFields) > 0 {
+		if _, ok := translate[sel.X.(*ast.Ident).Obj.Decl]; currentFunc == initialFunc || ok {
+			fmt.Printf("%v:Field %s used, but it was never selected\n", fset.Position(sel.X.Pos()),
+				fmt.Sprint(sel.X, `.Data.`, expr.Sel))
+		}
+	}
+	return false
+}
