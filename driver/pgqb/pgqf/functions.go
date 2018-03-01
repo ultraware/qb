514fe3dc2ae34e5acc84d6662ec4932fd44e1362
NiseVoid <nisevoid@gmail.com>
2018-03-01 11:20:26 +0100

    Add ArrayAgg to pgqf

--- driver/pgqb/pgqf/functions.go
+++ driver/pgqb/pgqf/functions.go
@@ -9,3 +9,8 @@ import (
 func Excluded(f qb.QueryStringer) qb.Field {
 	return qf.NewCalculatedField(`EXCLUDED.`, f)
 }
+
+// ArrayAgg is the postgres-specific function array_agg
+func ArrayAgg(f qb.Field) qb.Field {
+	return qf.NewCalculatedField(`array_agg(`, f, `)`)
+}
