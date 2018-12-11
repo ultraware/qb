9bebd65cd02c34bbb7e29725b644791f9d69a12c
NiseVoid <nisevoid@gmail.com>
2018-12-11 15:01:07 +0100

    Move alias to a separate file

---
+++ alias.go
@@ -0,0 +1,52 @@
+package qb
+
+import "strconv"
+
+type noAlias struct{}
+
+func (n *noAlias) Get(_ Source) string {
+	return ``
+}
+
+// NoAlias returns no alias.
+// This function is not intended to be called directly
+func NoAlias() Alias {
+	return &noAlias{}
+}
+
+type aliasGenerator struct {
+	cache    map[Source]string
+	counters map[string]int
+}
+
+// AliasGenerator returns an incrementing alias for each new Source.
+// This function is not intended to be called directly
+func AliasGenerator() Alias {
+	return &aliasGenerator{make(map[Source]string), make(map[string]int)}
+}
+
+func (g *aliasGenerator) Get(src Source) string {
+	if src == nil {
+		return ``
+	}
+
+	if v, ok := g.cache[src]; ok {
+		return v
+	}
+
+	new := g.new(src)
+	g.cache[src] = new
+	return new
+}
+
+func (g *aliasGenerator) new(src Source) string {
+	a := src.aliasString()
+
+	g.counters[a]++
+
+	if g.counters[a] == 1 {
+		return a
+	}
+
+	return a + strconv.Itoa(g.counters[a])
+}
