97818c2c0cc2cc3af8883ab5e56e07f637e00fae
Robin Knaapen <robin@ultraware.nl>
2026-04-21 17:21:54 +0200

    Update golangci-lint

--- qbdb/types.go
+++ qbdb/types.go
@@ -114,7 +114,7 @@ func (db *db) Begin() (Tx, error) {
 // BeginTx starts a transaction
 func (db *db) BeginTx(c context.Context, o *sql.TxOptions) (Tx, error) {
 	rawTx, err := db.DB.BeginTx(c, o)
-	return &tx{queryTarget{rawTx, db.queryTarget.driver, db.queryTarget.debug}, rawTx}, err
+	return &tx{queryTarget{rawTx, db.driver, db.debug}, rawTx}, err
 }
 
 // MustBegin is the same as Begin, but it panics if an error occurred
